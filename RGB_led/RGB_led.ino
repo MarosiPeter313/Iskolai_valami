4cba9d4c56d31ea4f44f1510ec1f0ea38525c957
Peter <Tszta@gmail.com>
2021-10-07 12:43:41 +0200

    led test

--- RGB_led/RGB_led.ino
+++ RGB_led/RGB_led.ino
@@ -1,7 +1,26 @@
 int r=9 ;
-int b=20 ;
-int v= 7 ;
+int b=10 ;
+int v= 11 ;
 void setup() {
+  pinMode (r, OUTPUT) ;
+  pinMode (b, OUTPUT) ;
+  pinMode (v, OUTPUT) ;
+
+  digitalWrite (r,1) ;
+  delay (500);
+
+  digitalWrite (r,1) ;
+  digitalWrite (b,1) ;
+  delay (500) ;
+  
+  digitalWrite (b,0) ;
+  digitalWrite (v,1) ;
+  delay (500) ;
+  
+  digitalWrite (r,1) ;
+  digitalWrite (b,1) ;
+  delay (500) ;
+  
   // put your setup code here, to run once:
 
 }
